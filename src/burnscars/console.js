3ba1d83d1e2f303bb2fec0c45e0474a030434c66
dmp42 <olivier@webitup.fr>
2012-08-05 02:49:42 +0200

    Console work

--- src/burnscars/console.js
+++ src/burnscars/console.js
@@ -2,84 +2,50 @@
 
 'use strict';
 
+// if (!window['console'] /*@cc_on || @_jscript_version <= 9 @*/ || !(function() {
+//   var ok = true;
+//   var props = [
+//     'log', 'debug', 'info', 'warn', 'error', 'assert', 'dir', 'dirxml', 'exception', 'time', 'timeEnd', 'table',
+//     'clear', 'trace', 'group', 'groupCollapsed', 'groupEnd', 'timeStamp', 'profile', 'profileEnd', 'count'
+//   ];
+//   for (var x = 0; x < props.length; x++)
+//     ok &= !!window.console[x];
+//   return ok;
+// })()) {
+
+// }
+
+// if (!document.getElementById('firebug-lite')) {
+//   // Include the script
+//   var script = document.createElement('script');
+//   script.type = 'text/javascript';
+//   script.id = 'firebug-lite';
+//   // If you run the script locally, point to /path/to/firebug-lite/build/firebug-lite.js
+//   script.src = 'https://getfirebug.com/firebug-lite.js';
+//   // If you want to expand the console window by default, uncomment this line
+//   //document.getElementsByTagName('HTML')[0].setAttribute('debug','true');
+//   document.getElementsByTagName('HEAD')[0].appendChild(script);
+//   setTimeout(function() { log(Array.prototype.slice.call(arguments)); }, 2000);
+// }
+// else {
+//   // FBL was included but it hasn't finished loading yet, so try again momentarily
+//   setTimeout(function() { log(Array.prototype.slice.call(arguments)); }, 500);
+// }
+
 // Useless first attempt
 // http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 
 // Better, more complete thingie
 // http://benalman.com/projects/javascript-debug-console-log/
 // http://benalman.com/code/projects/javascript-debug/docs/files/ba-debug-js.html
+// https://github.com/cowboy/javascript-debug/tree/v0.4
 
 // Seems quite risky
 // https://github.com/cpatik/console.log-wrapper
-
-// Fill-up the void, possibly
-// (function() {
-//   // Should be totally empty for no-debug
-//   var replace = function(){
-//     if(console && console.log)
-//       try{
-//         console.log.apply(console, arguments);
-//       }catch(e){
-//         for(var i in arguments)
-//           console.log(arguments[i]);
-//       }
-//   };
-//   var se = {
-//     log: replace,
-//     debug: replace,
-//     info: replace,
-//     warn: replace,
-//     error: replace,
-//     trace: replace
-//   };
-//   if (!('console' in window))
-//     window.console = se;
-
-//   // @patch IE8 (debug not in there... :())
-//   for (var i in se)
-//     if (!(i in window.console))
-//       window.console[i] = se[i];
-// })();
-
-// // Verbosity controller
-// (function() {
-//   var c = window.console;
-//   var e = {
-//     'DEBUG': 1,
-//     'LOG': 2,
-//     'INFO': 4,
-//     'WARN': 8,
-//     'ERROR': 16,
-//     'TRACE': 32,
-//     'ALL': 63
-//   };
-
-//   // console.time('start');
-//   for (var i in e) {
-//     c[i] = e[i];
-//     (function() {
-//       var meth = c[i.toLowerCase()];
-//       var level = c[i];
-//       c[i.toLowerCase()] = function() {
-//         if (c.VERBOSITY & level){
-//           // var args = Array.prototype.slice(arguments);
-//           // args.push(Date.now());
-//           // console.timeEnd('start');
-//           // console.timeEnd('previous');
-//           // console.time('previous');
-//           // Might very well crash IE bitch
-//           try{
-//             meth.apply(c, arguments);
-//           }catch(e){
-//             for(var i in arguments)
-//               meth(arguments[i]);
-//           }
-//         }
-//       };
-//     })();
-//   }
-//   c.VERBOSITY = c.INFO | c.WARN | c.ERROR | c.LOG | c.DEBUG | c.TRACE;
-// })();
+//
+// Looks really better
+// https://github.com/kayahr/console-shim
+// assuming we can spoof in firebuglite
 
 
 /**#nocode-*/
