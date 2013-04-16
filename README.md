f61b043fc400fee5ee807cdd0a22653757153bf9
dmp42 <olivier@webitup.fr>
2013-04-16 22:33:01 +0200

    Avoid disaster again

--- README.md
+++ README.md
@@ -1,249 +1,48 @@
-spitfire.js
-===========
+Spitfire.js
+=============
 
-Javascript shim / loader framework.
-
-Making things behave since 1976.
-
-Overview
+About
 -------------
 
-There exist numerous projects providing "shims" for various "aspects" of the modern web.
+Javascript shim / loader framework. Making things behave since 1976.
 
-Now, these are fragmented in multiple projects, and usually provided in a monolithic/raw form.
-We wanted a solution "ready-to-go", providing a very simple javascript file and API able to runtime detect browsers bugs and missing features, and load the best, appropriate "shims", ideally free from the chosen loader backend (require, lab).
-
-The 10 seconds API
+For the impatients
 -------------
 
-Get the content of the "lib" folder, and make it available on your server as "/whatever/lib". Include "spitfire-lab-min.js" in your page.
+Loren ipsum.
 
-
-```
-<!doctype html>
-<html>
-<head>
-  <script src="//base-url-to-spitfire-lib-folder/spitfire-lab-min.js"></script>
-  <script type="text/javascript">
-    var shims = Spitfire.boot();
-    var baseUri = Spitfire.loader.base('spitfire-lab');
-    for(var x = 0; x < shims.length; x++)
-      Spitfire.loader.script(baseUri + '/' + shims[x]);
-    Spitfire.loader.wait(function(){
-      // Ready to enjoy ES5, console, json, xhr...
-      console.warn(" [spitfire.js] all shims loaded");
-    });
-  </script>
-</head>
-<body>
-</body>
-</html>
-```
-
-Now, you can enjoy in any browser (at least, that's the purpose):
-- es5 support (partial, but most of the useful stuff)
-- proper Date support
-- json
-- XMLHttpRequest
-- console
-- localStorage
-- geolocation API
-- others coming
-
-
-Advanced API: Spitfire
+Problem
 -------------
 
-```javascript
-// Add a new shim, always to be used
-Spitfire.add({
-  test: !Function.something,
-  uri: 'path_to_shim.js'
-}, Spitfire.SAFE);
+Loren ipsum.
 
-// Add a new shim with an inline patch, to be used if a specific category is requested
-Spitfire.add({
-  test: !Function.somethingElse,
-  patch: function(){
-    Function.somethingElse = function(){
-      console.warn("Haha!");
-    };
-  }
-}, 'SomeCategory');
-
-// Ask for a specific category
-Spitfire.use('SomeCategory');
-
-// Use UNSAFE shims (shims that do not provide actual functionality, just named props)
-Spitfire.use(Spitfire.UNSAFE);
-// Enforce JSON replacement shim instead of native implementation, no matter what
-Spitfire.use(Spitfire.JSON);
-// Enforce XHR replacement shim instead of native implementation, no matter what
-Spitfire.use(Spitfire.XHR);
-
-// To use non-minified versions of the shims, pass true
-var shims = Spitfire.boot(true);
-
-// Load your shims, then, the way you want
-```
-
-Advanced API: Spitfire/loader
--------------
-```javascript
-var baseUri = Spitfire.loader.base('spitfire');
-for(var x = 0; x < shims.length; x++)
-  Spitfire.loader.script(baseUri + '/' + shims);
-
-// Use the bundled loader to load other resources (API similar to labjs)
-
-Spitfire.loader.script('something.js')
-  .script('other.js')
-  .wait(function(){
-    // other will be executed before last 
-  })
-  .script('last.js')
-  .style('stylesheet.css')
-  .wait(function(){
-    // Everything loaded
-  });
-
-var ld = Spitfire.loader.fork;
-ld.script('to_be_loaded_separately.js');
-
-```
-
-See the jsdoc documentation for more.
-
-
-Advanced API: gulliver.js
+Solution
 -------------
 
-Gulliver is a MINIMALIST loader (under 1kB), meant for ealy stage boot.
-If you don't understand what all that means, and the implications, you probably don't need it.
+Loren ipsum.
 
-How to use:
-
-
-Advanced embedding strategies
+How to use
 -------------
 
-If you prefer to use modules (and requirejs), just embed instead "spitfire-require-min.js".
+Loren ipsum.
 
-You can also use "spitfire-yahoo-min.js", "spitfire-head-min.js", or "spitfire-yepnope-min.js" for alternative loaders.
-
-The API is exactly the same - but the bundled loader differ.
-Note that if "require" exists in the global scope, we switch to a module API instead - you must then require 'Spitfire' and 'Spitfire/loader' instead of namespaces.
-
-If you prefer to use your own copy of your favorite loader (assuming its supported - that is: labjs, requires, yahoo loader, yepnope and headjs), just embed the "spitfire-min.js" file which just contains the API and wrapper, and no actual loader backend.
-
-If you don't care at all about the loader API, just embed "shimer-min.js" to get the Spitfire module/namespace with no loader (it's then your responsibility to load the uris).
-
-If you are looking for a as-slick-as-possible minimal bootstrap loader, take a look at gulliver (see documentation).
-
-```
-gulliver(function(){
-  // The targetted script has been loaded
-}, "uri_of_the_target_script_to_load");
-
-```
-
-If you want don't care about conditional testing or loading at all, and just want a shim-it-all file, use the burnscars-min.js file directly. 
-
-
-About the shims
+API
 -------------
 
-Very few original code is provided here.
+Loren ipsum.
 
-Most of the actual shiming code is provided by third-party library and sources, namely:
-- es5-shim: https://github.com/kriskowal/es5-shim/
-- json3: http://bestiejs.github.com/json3/
-- geolocation: http://www.calormen.com/polyfill/geo.js
-- XMLHttpRequest
-- Console
-- localStorage
-- http://developer.mozilla.org
-- others
-
-See the pukefile and individual burnscars/ files for information.
-All of them use a MIT compatible license, or public domain.
-
-The loader API is largely inspired by labjs.
-
-About the tests
+How to build
 -------------
 
-Uses jasmine.
-
-Bundles in es5-shim tests as well.
-
-You need to build (puke all) to have the tests deployed.
-
-Navigate your browser to lib/tests/All.html
-
-It will default to use loader-lab, unless you add params in the url to specify another loader
-(eg: #loader-require).
-
-The following additional fragments options are supported in the url:
-- use shimer as well: #use-spitfire
-- use shimer with unsafe shims: #use-spitfire-full
-- to use ES5-shim only: #use-es5
-- to use minified versions of the shims: #use-min
-
-You can combine any of these (though use-spitfire implies use-es5).
-
-About the bugs
--------------
-
-Yahoo loader doesn't behave.
-If you really use it, speak-up so we consider to fix.
+Loren ipsum.
 
 How to contribute
 -------------
 
-Issues, or forks + pull requests.
+Loren ipsum.
 
-Be sure to add tests for what you want "shimed".
-
-If the problem is an ES5 feature / bug, get upstream instead. Same goes for console, XHR, and JSON3.
-
-Versioning and API
+License
 -------------
 
-We use semantic versioning (semver), and the uri by default contain only the MAJOR.MINOR part of the version.
-
-Existing shims fixing / enhancing will trigger a REVISION bump.
-
-New shims will trigger a MINOR bump.
-
-Changes to the Spitfire API will trigger a MAJOR bump.
-
-Build system change and undocumented features change will NOT trigger a version bump.
-
-
-Server availability
--------------
-
-Spitfire will be made available, prebuilt, under a jsboot domain - though, unless you use the jsBoot stack, you should rather use it on your own server infrastructure...
-
-
-Technology
--------------
-
-We use puke (https://github.com/webitup/puke), a (inhouse) versatile python build system.
-
-We also depend on airstrip.js (https://github.com/jsBoot/airstrip) in order to 
-provide dependencies at build time - though you don't need to build it yourself and won't depend
-on it at runtime (airstrip provides access to the third-party libraries that are copied in the build result).
-
-How to build it yourself
--------------
-
-- you need a working python + pip environment
-- install puke: `pip install puke`
-- clone: `git clone https://github.com/jsBoot/spitfire.js`
-- build it as-is: `cd spitfire.js; puke all`
-
-You can customize the build directories output by customizing a config-USERNAME-OSNAME.yaml (see that file for inspiration).
-
+Loren ipsum.
 
--- pukefile.py
+++ pukefile.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env puke
 # -*- coding: utf8 -*-
 
-global PH
-import helpers as PH
+global help
+from helpers import Helpers as help
 import re
 import json
 
 @task("Default task")
 def default():
-  # Cache.clean()
   executeTask("build")
-  executeTask("tests")
   executeTask("deploy")
 
 @task("All")
@@ -28,216 +26,68 @@ def all():
 
 @task("Wash the taupe!")
 def clean():
-  PH.cleaner()
+  Cache.clean()
+  help.cleaner()
 
 @task("jsDocking")
 def doc():
-  list = FileList(Yak.build_root, filter = "*gulliver.js,*loader.js,*shimer.js")
+  list = FileList(Yak.paths['build'], filter = "*.js")
   # jsdoc3(list, Yak.doc_root + "/jsdoc3.json")
-  d = FileSystem.abspath(Yak.doc_root)
+  d = FileSystem.abspath(Yak.paths["doc"])
   jsdoc3(list, "%s/gristaupe.json" % d)
   jsdoc3(list, "%s/html" % d, template = "templates/default")
 
 @task("Lint")
 def lint():
-  PH.linter("src")
+  help.linter("src")
 
 @task("Hint")
 def hint():
-  # Tests can't be hinted - too many dirty things in there
-  PH.hinter("src", excluding = "*tests*,*strict.js")
+  help.hinter("src")
 
 @task("Flint")
 def flint():
-  PH.flinter("src")
-
-@task("Deploy package")
-def deploy():
-  PH.deployer(True)
+  help.flinter("src")
 
 @task("Minting")
 def mint():
-  # Yahoo and yep don't support strict
-  # Minting specs is a bad idea, as some code simpifications defeat some of them
-  PH.minter(Yak.build_root, filter = "*yahoo.js,*yepnope.js", excluding="*/specs/*,*/es5/*", strict = False)
-  PH.minter(Yak.build_root, excluding = "*yahoo*,*yepnope*,*/specs/*,*/es5/*")
+  help.minter(Yak.paths['build'], strict = True)
+  # Some dirty code might not pass strict
+  # help.minter(Yak.paths['build'], strict = False)
 
 @task("Stats report deploy")
 def stats():
-  PH.stater(Yak.build_root)
+  help.stater(Yak.paths['build'])
 
 @task("Tests building")
 def tests():
   # ============================
   # Build tests
   # ============================
-    sed = Sed()
-    PH.replacer(sed)
+  sed = Sed()
+  help.replacer(sed)
 
-    # Get the remoty shims first
-    remoty = Yak.links["airstrip"]["url"].split('/')
-    remoty.pop()
-    remoty = '/'.join(remoty)
-    sed.add('{AIRSTRIP-BASE}', remoty)
-
-    # Python doesn't support SNI :(((( - SUCKS!
-    # https://github.com/kennethreitz/requests/issues/749
-    yam = json.loads(http.get(Yak.links["airstrip"]["url"], verify=False).text)[Yak.links["airstrip"]["version"]]
-
-    # Prep-up the jasmine links
-    for i in yam['jasmine']:
-      if (Yak.istrunk and i.find('trunk') != -1) or ((not Yak.istrunk) and i.find('stable') != -1):
-        if i.find('.css') != -1:
-          sed.add('{SPIT-JASCSS}', remoty + '/' + i.replace('.css', ''))#.replace('.css', '-min.css'))
-        elif i.find('.html') != -1:
-          sed.add('{SPIT-JASHTML}', remoty + '/' + i.replace('.js', ''))#.replace('.js', '-min.js'))
-        else:
-          sed.add('{SPIT-JAS}', remoty + '/' + i.replace('.js', ''))#.replace('.js', '-min.js'))
-
-
-    list = FileList("src/tests", filter="*.js,*.html", exclude="*xxx*")
-    deepcopy(list, Yak.build_root + '/tests', replace=sed)
-
-    # Can't access from raw github using IE (mimetype mismatch, bitch)
-    list = [
-      "https://raw.github.com/kriskowal/es5-shim/master/tests/helpers/h.js",
-      "https://raw.github.com/kriskowal/es5-shim/master/tests/helpers/h-matchers.js",
-      "https://raw.github.com/kriskowal/es5-shim/master/tests/spec/s-array.js",
-      "https://raw.github.com/kriskowal/es5-shim/master/tests/spec/s-function.js",
-      "https://raw.github.com/kriskowal/es5-shim/master/tests/spec/s-string.js",
-      "https://raw.github.com/kriskowal/es5-shim/master/tests/spec/s-object.js",
-      "https://raw.github.com/kriskowal/es5-shim/master/tests/spec/s-date.js"
-    ]
-    deepcopy(list, Yak.build_root + '/tests/es5/');
+  list = FileList(Yak.paths['tests'], filter="*.js,*.html", exclude="*xxx*")
+  deepcopy(list, Yak.paths['build'] + '/tests', replace=sed)
 
 
 @task("Build package")
 def build():
   # ============================
-  # Basic stuff
+  # Very basic build
   # ============================
-    sed = Sed()
-    PH.replacer(sed)
 
-    # Get the remoty shims first
-    remoty = Yak.links["airstrip"]["url"].split('/')
-    remoty.pop()
-    remoty = '/'.join(remoty)
-    yam = json.loads(http.get(Yak.links["airstrip"]["url"], verify=False).text)[Yak.links["airstrip"]["version"]]
-
-    # Allow to test separate loaders as well
-    # print yam
-    # for i in yam['yahoo']:
-    #   if (i.find('trunk') == -1) or istrunk:
-    #     sed.add('{SPIT-YAHOO}', remoty + '/' + i)#.replace('.css', '-min.css'))
-    # print yam
-    # for i in yam['head']:
-    #   if (i.find('trunk') == -1) or istrunk:
-    #     sed.add('{SPIT-HEAD}', remoty + '/' + i)#.replace('.css', '-min.css'))
+  sed = Sed()
+  help.replacer(sed)
+  deepcopy(FileList("src", exclude = "*tests*"), Yak.paths['build'], replace = sed)
 
 
-  # ============================
-  # Get the external shims
-  # ============================
-    allshims = FileList("src/burnscars", filter="*.js", exclude="*xxx*")
-    for (k, elem) in {
-      'json3': 'json3',
-      'xmlhttprequest': 'xmlhttprequest',
-      'es5-shim': 'es5',
-      'es5-sham': 'es5',
-      'console': 'console'
-    }.items():
-      candidate = ''
-      for i in yam[elem]:
-        if (Yak.istrunk and i.find('trunk') != -1) or ((not Yak.istrunk) and i.find('stable') != -1) and (i.find(k) != -1):
-          candidate = remoty + '/' + i
-          allshims.merge([candidate])
-          break
-      combine(['src/strict.js', candidate], '%s/burnscars/%s.js' % (Yak.build_root, k), replace=sed)
-      sed.add('{SPIT-%s}' % k.upper(), k)
-
-  # ============================
-  # Get local shims and vanilla flavors
-  # ============================
-    deepcopy(FileList("src/burnscars", filter="*.js,*.html", exclude="*xxx*"), Yak.build_root + '/burnscars', replace=sed)
-    deepcopy("src/shimer.js", Yak.build_root, replace=sed)
-    deepcopy("src/loader.js", Yak.build_root, replace=sed)
-    deepcopy("src/gulliver.js", Yak.build_root, replace=sed)
-    combine(["src/loader.js", "src/shimer.js"], Yak.build_root + '/spitfire.js', replace=sed)
-
-  # ============================
-  # Prep-up the manifest
-  # ============================
-    shortversion = Yak.package['version'].split('-').pop(0).split('.')
-    shortversion = shortversion[0] + "." + shortversion[1]
-    spitroot = Yak.package['name'] + "/" + shortversion
-
-    # All in one shim
-    combine(allshims, Yak.build_root + '/burnscars.js', replace=sed)
-
-  # ============================
-  # Build tainted loaders
-  # ============================
-    for elem in ['lab', 'head', 'require']:
-      candidate = ''
-      for i in yam[elem]:
-        if (Yak.istrunk and i.find('trunk') != -1) or ((not Yak.istrunk) and i.find('stable') != -1):
-          candidate = remoty + '/' + i
-          break
-      combine(['src/strict.js', candidate, 'src/loader.js'], '%s/loader-%s.js' % (Yak.build_root, elem), replace=sed)
-
-    # YepNope and Yahoo are crap and don't support strict mode
-    candidate = ''
-    elem = 'yahoo'
-    for i in yam[elem]:
-      if (Yak.istrunk and i.find('trunk') != -1) or ((not Yak.istrunk) and i.find('stable') != -1):
-        candidate = remoty + '/' + i
-        break
-    combine([candidate, 'src/loader.js'], '%s/loader-%s.js' % (Yak.build_root, elem), replace=sed)
-
-    candidate = ''
-    elem = 'yepnope'
-    for i in yam[elem]:
-      if (Yak.istrunk and i.find('trunk') != -1) or ((not Yak.istrunk) and i.find('stable') != -1):
-        candidate = remoty + '/' + i
-        break
-    combine([candidate, 'src/loader.js'], '%s/loader-%s.js' % (Yak.build_root, elem), replace=sed)
-
-
-  # ============================
-  # Build all-in-one
-  # ============================
-    for elem in ['lab', 'head', 'require', 'yepnope', 'yahoo']:
-      combine(['%s/loader-%s.js' % (Yak.build_root, elem), '%s/shimer.js' % Yak.build_root], '%s/spitfire-%s.js' % (Yak.build_root, elem), replace=sed)
-
-  # ============================
-  # Build manifest itself
-  # ============================
-    description = {}
-    # Separate components
-    description["shimer"] = '%s/shimer.js' % spitroot
-    description["gulliver"] = '%s/gulliver.js' % spitroot
-    description["loader"] = '%s/loader.js' % spitroot
-    # Tainted loaders
-    for elem in ['lab', 'head', 'require', 'yahoo', 'yepnope']:
-      description["loader-%s" % elem] = "%s/loader-%s.js" % (spitroot, elem)
-
-    # Standalone shims that can be used bundled by build systems
-    description["json"] = '%s/burnscars/json3.js' % spitroot
-    description["xhr"] = '%s/burnscars/xmlhttprequest.js' % spitroot
-    description["es5"] = '%s/burnscars/es5-shim.js' % spitroot
-    description["console"] = '%s/burnscars/console.js' % spitroot
-    # All-in-one shim
-    description["burnscars"] = '%s/burnscars.js' % spitroot
-
-    # The actual spitfire (loader + shimer)
-    description["spitfire"] = '%s/spitfire.js' % spitroot
-    # And the tainted versions
-    for elem in ['lab', 'head', 'require', 'yahoo', 'yepnope']:
-      description["spitfire-%s" % elem] = "%s/spitfire-%s.js" % (spitroot, elem)
-
-
-    PH.describe(shortversion, "spitfire", description)
-
-
+@task("Deploy package")
+def deploy():
+  # Libraries usually have a versioned path (True)
+  # help.deployer(True)
+  # Sites or apps dont
+  help.deployer(Yak.paths['build'], False)
+  # In case you wanna deploy dependencies as well
+  help.deployer('dependencies', False)
 
