cafc9bc160a624ba18e3e4db76b02b9cd083b19d
dmp42 <olivier@webitup.fr>
2013-09-04 16:57:14 +0200

    Forgot the builded spec

--- dist/tests/specs/Globals.js
+++ dist/tests/specs/Globals.js
@@ -40,7 +40,7 @@
         });
       });
 
-      it('undefined should be read-only (javascript 1.8.5)', function() {
+      xit('undefined should be read-only (javascript 1.8.5)', function() {
         runs(function() {
           try {
             undefined = 'ass';
@@ -104,7 +104,7 @@
           expect(Number.NaN.constructor).toBe(Number);
         });
       });
-      it('NaN is read-only', function() {
+      xit('NaN is read-only', function() {
         runs(function() {
           try {
             NaN = 'ass';
