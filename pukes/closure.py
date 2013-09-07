b9b2e36644632d998e2e339f5e9149b06a2af2a8
dmp42 <olivier@webitup.fr>
2013-09-07 17:31:23 +0200

    Pepping8

--- pukes/closure.py
+++ pukes/closure.py
@@ -1,5 +1,6 @@
 import puke2 as puke
 
+
 class gjsLint:
 
     """ jshint helper to get of the top
@@ -11,21 +12,22 @@ class gjsLint:
     def go(self, list):
 
         # tags = 'homepage,ignore,requires,namespace,property,static,default,location,copyright,memberOf,lends,fileOverview'
-        # # This covers jsdoc3 as well
+        # This covers jsdoc3 as well
         # tags += 'description,event,exception,exports,fires,global,inner,instance,var,memberof,mixes,mixin,arg,argument,readonly,since,todo,public'
 
         tags =  'file,summary,version,copyright,name,location,module,todo,abstract,member,function,\
 example,constant,returns,author'
         return self.com(
-          '--jslint_error', 'blank_lines_at_top_level',
-          '--jslint_error', 'indentation',
-          '--jslint_error', 'no_braces_around_inherit_doc',
-          '--jslint_error', 'braces_around_type',
-          '--jslint_error', 'optional_type_marker',
-          '--jslint_error', 'unused_private_members',
-          '--max_line_length', '100',
-          '--custom_jsdoc_tags', tags,
-          *list)
+            '--jslint_error', 'blank_lines_at_top_level',
+            '--jslint_error', 'indentation',
+            '--jslint_error', 'no_braces_around_inherit_doc',
+            '--jslint_error', 'braces_around_type',
+            '--jslint_error', 'optional_type_marker',
+            '--jslint_error', 'unused_private_members',
+            '--max_line_length', '100',
+            '--custom_jsdoc_tags', tags,
+            *list)
+
 
 class fixJsStyle:
 
@@ -39,12 +41,12 @@ class fixJsStyle:
         tags =  'file,summary,version,copyright,name,location,module,todo,abstract,member,function,\
 example,constant,returns,author'
         return self.com(
-          '--jslint_error', 'blank_lines_at_top_level',
-          '--jslint_error', 'indentation',
-          '--jslint_error', 'no_braces_around_inherit_doc',
-          '--jslint_error', 'braces_around_type',
-          '--jslint_error', 'optional_type_marker',
-          '--jslint_error', 'unused_private_members',
-          '--max_line_length', '100',
-          '--custom_jsdoc_tags', tags,
-          *list)
+            '--jslint_error', 'blank_lines_at_top_level',
+            '--jslint_error', 'indentation',
+            '--jslint_error', 'no_braces_around_inherit_doc',
+            '--jslint_error', 'braces_around_type',
+            '--jslint_error', 'optional_type_marker',
+            '--jslint_error', 'unused_private_members',
+            '--max_line_length', '100',
+            '--custom_jsdoc_tags', tags,
+            *list)
